c02b13bf4c36169cb6f3896f069948bc84860fc2
devZ_VikAsH <pvickey663@gmail.com>
2023-07-03 17:45:17 +0530

    Update fastReview.ts

--- fastReview.ts
+++ fastReview.ts
@@ -30,18 +30,46 @@
 
 // customerLogger(customer);
 
-
-
 // interface Pair<T, U> {
 //     name: T
 //     age(): U
 // }
 
-
 // let p: Pair<string, void> = { name: "vik", age: () => console.log("hello") };
 // console.log('p', p)
 
+// interface ElementChecker {
+//     <T>(items: T[], toBeChecked: T, atIndex: number): boolean;
+// }
 
-interface ElementChecker {
-    <T>(items: T[], toBeChecked: T, atIndex: number): boolean;
-}
+// function checker<T>(item: T[], toCheck: number, idx: number): boolean {
+//     return item[idx] == toCheck;
+// }
+
+// let items= [1,3,5,7];
+// let newChecker : boolean = checker(items, 1, 0);
+
+// console.log('newChecker', newChecker)
+
+// interface States<R> {
+//     [state: string]: R;
+// }
+
+// let s: States<boolean> = { enables: !!'false' };
+
+
+// class GenericNumber<T>{
+//     zeroValue: T;
+//     // add: (x: T, y: T) => T;
+//     add(a: number, b: number): number {
+//         return a + b;
+//     }
+// }
+
+// let myGen = new GenericNumber<number>();
+
+// // myGen.add = function (x, y) {
+// //     return x + y;
+// // }
+
+// console.log('myGen.add(34,43)', myGen.add(34, 43))
